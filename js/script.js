f3e4f2c394cbba65e1ad21504324b9acd150e4a1
Maurocazzato <maurizzio.125@gmail.com>
2023-12-07 13:12:47 +0100

    creazione div + messaggio + immgaine con vue.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            messaggio: "Hello Vue!",
+            mieClassi: "text",
+            imageUrl: "img/vue-js.png",
+            mieClassi: "img1",
+            
+        }
+    }
+
+
+
+
+}).mount('#app');
